7d46be1d5c664333637879dc8b803587a0e50e1d
Cristian Eduardo <cristianeduardo.dev@gmail.com>
2021-12-18 11:08:52 -0300

    Learning about Java inversion of control

---
+++ src/com/luv2code/springdemo/SoccerCoach.java
@@ -0,0 +1,8 @@
+package com.luv2code.springdemo;
+
+public class SoccerCoach implements Coach {
+	@Override
+	public String getDailyWorkout() {
+		return "Pass the ball to Cristiano Ronaldo 'Eu sou o milior'.";
+	}
+}
